513d0fef2aba9e0abbc4fc048c3ab6714304e827
Jason <quizathaderat@yahoo.com>
2015-08-11 23:24:53 -0700

    Add toTitleCase method

--- src/main/java/TitleCase.java
+++ src/main/java/TitleCase.java
@@ -1,8 +1,14 @@
 public class TitleCase {
   public static void main(String[] args) {
   }
-  public String toTitleCase(String yourPhrase){
-
-   return yourPhrase;
+  public String toTitleCase(String yourPhrase) {
+    String word = "";
+    String[] words = yourPhrase.split(" ");
+    for (Integer i = 0; i < words.length; i++) {
+      Character ch = Character.toUpperCase(words[0].charAt(0));
+      word = ch + words[i].substring(1);
+      return word;
+    }
+    return word;
  }
 }
